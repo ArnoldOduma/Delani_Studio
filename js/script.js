c4f9b5a975001351287f608fde4b707a71371337
Arnold Oduma <arnoldcliff98@gmail.com>
2019-05-05 19:47:00 +0300

    Added the wow.js library, included the uncompressed j-query, added the animate.css plugin

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
   $(".text").hide();
-  $(".weDo-cont").click(function() {
+  $(".weDo-cont").click(function () {
     $(this).children(".text").slideToggle(500);
     $(this).children(".hide-img").toggle(500);
+  });
+
+  $(".port_desc").hide();
+  $(".port_cont").hover(function () {
+    $(this).children(".port_desc").fadeToggle("fast", "linear");
   });
 
 
