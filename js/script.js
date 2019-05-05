007cdaa3e84368380288762bf6f172c8fe2deeef
Arnold Oduma <arnoldcliff98@gmail.com>
2019-05-05 19:53:44 +0300

    Added the message class to display error and success messages in form.. Created a funtion to validate form inputs before submit

--- js/script.js
+++ js/script.js
@@ -11,6 +11,18 @@ $(document).ready(function () {
     $(this).children(".port_desc").fadeToggle("fast", "linear");
   });
 
-
+  $("#submission").submit(function(event){
+    var data_1 = $("input:first").val();
+    var data_2 = $("input#mail").val();
+    if(data_1 && data_2 != ""  ){
+      $(".message").text("Thank you " + data_1 + " Your message has been received");
+      $(".message").removeClass("error");
+      $(".message").addClass("success");
+    }else{
+      $(".message").text("Error!!! Please fill in all the details correctly");
+      $(".message").addClass("error");
+    }
+    event.preventDefault();
+  });
 
 });
