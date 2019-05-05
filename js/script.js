9d18c451eedd8e14084c995fb8effc7ea418160a
Arnold Oduma <arnoldcliff98@gmail.com>
2019-05-05 20:13:00 +0300

    Added a bounce effect on the mouse icon

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
 $(document).ready(function () {
 
-  $(".text").hide();
   $(".weDo-cont").click(function () {
     $(this).children(".text").slideToggle(500);
     $(this).children(".hide-img").toggle(500);
   });
 
-  $(".port_desc").hide();
   $(".port_cont").hover(function () {
     $(this).children(".port_desc").fadeToggle("fast", "linear");
   });
